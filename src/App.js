47c33e93c4ca1ababdb8a3c8c0b789244cf56984
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-09-19 18:13:04 +0900

    ログイン・新規登録がとりあえずできた気がする

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Layout from "./pages/Layout";
 import TimeLine from "./pages/TimeLine";
 import EditProfile from "./pages/EditProfile";
-import RegisterUser from "./pages/RegisterUser";
+import Register from "./pages/Register";
 import ShopList from "./pages/ShopList";
+import Login from "./pages/Login";
+import LoginRegister from "./pages/LoginRegister"
 
 export default function () {
   return (
@@ -15,8 +18,9 @@ export default function () {
       <Layout>
         <Route exact path="/" component={TimeLine}></Route>
         <Route path="/EditProfile" component={EditProfile}></Route>
-        <Route path="/RegisterUser" component={RegisterUser}></Route>
         <Route path="/ShopList" component={ShopList}></Route>
+        <Route path="/Login" component={() => <LoginRegister feature="login" />}></Route>
+        <Route path="/Register" component={() => <LoginRegister feature="register" />}></Route>
       </Layout>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,13 @@ export default class Layout extends React.Component {
   render() {
     return (
         <div>
-            <Link to="/">TimeLine</Link>
-            <Link to="ShopList">ShopList</Link>
-            <Link to="/EditProfile">EditProfile</Link>
-            <Link to="/RegisterUser">RegisterUser</Link>
+          <Link to="/">TimeLine</Link>
+          <Link to="ShopList">ShopList</Link>
+          <Link to="/EditProfile">EditProfile</Link>
+          <div className="right">
+            <Link to="/Login">Login</Link>
+            <Link to="/Register">Register</Link>
+          </div>
         </div>
     );
   }
