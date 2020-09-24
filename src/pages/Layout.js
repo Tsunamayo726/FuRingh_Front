db43e28403424b152cfad9af7bce9cb6b66b644d
Sagi Sorata <m18063@g.metro-cit.ac.jp>
2020-09-24 16:50:28 +0900

    不要なページの削除

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import Header from "../components/Header";
+import Timeline from "./TimeLine";
 export default class Layout extends React.Component {
   render() {
     return (
         <div>
             <Header />
-            {this.props.children}
-            
+            <Timeline/>
         </div>
     );
   }
