4457dc3c79149aa2509a484be75059fe94a544d2
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-09-24 18:25:35 +0900

    ReactでPostComponentした

---
+++ src/pages/PostComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import icon from "../content/testicon.png"
+import "./PostComponent.css"
+
+export default class PostComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    let user = props.username
+    user[0]==="@"||(user="@"+user)
+    this.state = {
+      username: user,
+      text: props.text,
+    }
+  }
+
+  render() {
+    return (
+      <div className="PostComponent">
+        <div className="upper">
+          <img src={icon} className="icon"/>
+          <div className="username">{this.state.username}</div>
+        </div>
+        <div className="downer">
+          <div className="text">
+            {this.state.text}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -1,11 +1,19 @@
 import React from "react";
+import PostComponent from "./PostComponent";
+import "./TimeLine.css"
 
 export default class TimeLine extends React.Component {
   render() {
     return (
-        <div>
-            <h1>TimeLine</h1>
-        </div>
+      <div className="timeline">
+        <h1>Timeline</h1>
+<PostComponent
+  username="sagi"
+  text="TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT,
+TEXTTEXTTEXTTEXTTEXTTEXT,
+TEXTTEXTTEXTTEXT."
+/>
+      </div>
     );
   }
 }
