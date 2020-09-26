4deacb3f331741e00c32abcb14f063f852393bf5
Sagi Sorata <m18063@g.metro-cit.ac.jp>
2020-09-26 11:55:37 +0900

    stateとデータ取得でのリスト化

--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -1,21 +1,41 @@
 import React from "react";
-import PostComponent from "./PostComponent";
+import api from "../api/apiCon";
+import { For } from 'react-loops'
+import PostComponent from "../components/PostComponent";
 import "./TimeLine.css"
 
 export default class TimeLine extends React.Component {
   constructor(props) {
     super(props)
     console.log("Timeline")
+    this.state = {
+      post_list:[1,2,3],
+    }
+  }
+
+
+  componentDidMount(){
+    api.get_timeline().then((result)=>{
+      console.log(result);
+
+      const component = <h1>hello world</h1>
+
+      this.setState({
+        post_list:result,
+      });
+    });
   }
 
   render() {
-    return (<div className="timeline">
-      <PostComponent
-        username="sagi"
-        text="TEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXTTEXT,
-TEXTTEXTTEXTTEXTTEXTTEXT,
-TEXTTEXTTEXTTEXT."
-/>
+    return (
+    <div className="timeline">
+      <ul>
+        <For of={this.state.post_list}>{item => 
+          <div>
+            <li>{item.text}</li>
+          </div>
+        }</For>
+      </ul> 
     </div>);
   }
 }
