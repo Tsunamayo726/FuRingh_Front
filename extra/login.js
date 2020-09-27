f02ba69d9f2d1e7350edda703dbc6f8fc2eb7f06
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-09-27 21:49:39 +0900

    ログインページ

---
+++ extra/login.js
@@ -0,0 +1,23 @@
+window.onload=()=>{
+  document.querySelector("div.form > form").onsubmit = (e) => {
+    const data = {
+      username: e.target.username.value,
+      password: e.target.password.value
+    }
+    console.log(data)
+    fetch('/api/v1/user/login', {
+      method: "POST",
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        document.querySelector("div.status").innerText = "OK"
+      });
+    return false
+  }
+}
