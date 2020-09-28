b091bf06b4cf0da040935ba2660f5a9030e0f727
Sagi Sorata <m18063@g.metro-cit.ac.jp>
2020-09-28 22:27:27 +0900

    <tbody>タグの追加

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -4,7 +4,12 @@ import "./Post.css"
 export default class Post extends Component {
   submit(e) {
     console.log("Submit form")
-    return true
+    console.log(e.target.itemname.value);
+    console.log(e.target.quantity.value);
+    console.log(e.target.price.value);
+    // console.log(e.target.textarea.value);
+
+    return false
   }
 
   render() {
@@ -15,6 +20,7 @@ export default class Post extends Component {
           onSubmit={this.submit}
           >
           <table>
+            <tbody>
               <tr className="itemname">
                 <th>品目</th>
                 <th><input required type="text" name="itemname" /></th>
@@ -27,8 +33,9 @@ export default class Post extends Component {
                 <th>価格</th>
                 <th><input required type="number" min="1" name="price" /></th>
               </tr>
+            </tbody>
           </table>
-          <textarea required className="textarea" placeholder="コメント"></textarea>
+          <textarea required className="textarea"  placeholder="コメント"></textarea>
           <br/>
           <input type="submit" value="投稿" />
         </form>
