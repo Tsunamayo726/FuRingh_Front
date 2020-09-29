337876e41182c33eb0d7922313a07658867f79fd
kazukazuprogram <33714346+kazukazuprogram@users.noreply.github.com>
2020-09-29 21:32:14 +0900

    とりま動くかもしれない

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,32 +3,48 @@ import {Link} from "react-router-dom";
 import "./Header.css"
 
 export default class Layout extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: "@______"
+    }
+    fetch("/api/v1/user/getmyinfo")
+    .then(response => response.json())
+    .then(json => {
+      console.log(json.name)
+      this.setState({username: json.name})
+    })
+  }
   render() {
     return (
       <div className="Header">
-        <div className="left">
+        <div className="logo">
           <Link to="/">
-              TimeLine
-          </Link>
-          <Link to="Post">
-              Post
-          </Link>
-          <Link to="ShopList">
-              ShopList
-          </Link>
-          <Link to="/EditProfile">
-              EditProfile
+            <img src="/logo.png" alt="Service Logo" />
           </Link>
         </div>
-        <div className="right">
-          <Link to="/Login">
-              Login
-          </Link>
-          <Link to="/Register">
-              Register
-          </Link>
-        </div>
+        <nav class="header-list">
+          <ul class="nav">
+            <li>
+              <Link to="/">タイムライン</Link>
+            </li>
+            <li>
+              <a href="profile.html">@{this.state.username}</a>
+            </li>
+            <li>
+              <a
+                onClick={()=>{
+                  fetch("/api/v1/user/logout").then((response)=>{
+                    console.log("Logout")
+                    window.location.pathname = "/login"
+                  })
+                  return false
+                }}
+                href="#">ログアウト</a>
+            </li>
+          </ul>
+        </nav>
       </div>
-    );
+    )
   }
 }
